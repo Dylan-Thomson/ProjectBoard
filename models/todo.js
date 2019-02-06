9d0c9e2aeefefe6569ce495605885dbf9a4175ea
Dylan Thomson <djthomson88@gmail.com>
2019-02-06 11:33:36 -0500

    Set up todo.js model

---
+++ models/todo.js
@@ -0,0 +1,40 @@
+const orm = require("../config/orm");
+
+const todo = {
+    all: callback => {
+        orm.selectAll("todos", result => {
+            callback(result);
+        });
+    },
+    create: (cols, vals, callback) => {
+        orm.insertOne("todos", cols, vals, result => {
+            callback(result);
+        });
+    },
+    update: (objColVals, condition, callback) => {
+        orm.updateOne("todos", objColVals, condition, result => {
+            callback(result);
+        });
+    }
+}
+
+function test() {
+    todo.all(result => {
+        console.log(result);
+    });
+    todo.create(["todo"], ["Sleep"], result => {
+        console.log(result);
+    });
+    todo.all(result => {
+        console.log(result);
+    });
+    todo.update({todo: "Cook dinner"}, "id = 4", result => {
+        console.log(result);
+    })
+    todo.all(result => {
+        console.log(result);
+    });
+}
+
+
+module.exports = todo;

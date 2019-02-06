156d69af2b684e8f2730d10f907e86d3833b7ad1
Dylan Thomson <djthomson88@gmail.com>
2019-02-06 11:16:44 -0500

    Set up database schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS projectboard_db;
+CREATE DATABASE projectboard_db;
+USE projectboard_db;
+
+CREATE TABLE todos (
+id INT(10) NOT NULL AUTO_INCREMENT,
+todo VARCHAR(100) NOT NULL,
+completed BOOLEAN,
+PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE projectboard_db;
+
+INSERT INTO todos (todo)
+VALUES ("Write code");
+
+INSERT INTO todos (todo)
+VALUES ("Cry");
+
+INSERT INTO todos (todo)
+VALUES ("Debug");

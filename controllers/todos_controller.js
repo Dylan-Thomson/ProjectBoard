a9e2821c71bde3362083873c5ad10065d27121ff
Dylan Thomson <djthomson88@gmail.com>
2019-02-08 13:17:30 -0500

    Some starter styles, click a To Do task to set to Completed

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -11,6 +11,23 @@ router.get("/", (req, res) => {
         console.log(handlebarsObj);
         res.render("index", handlebarsObj);
     });
-})
+});
+
+router.get("/api/todos/:id", (req, res) => {
+    const condition = "id = " + req.params.id;
+
+    todo.update(
+        {
+            completed: true
+        },
+        condition,
+        result => {
+            if(result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    )
+});
 
 module.exports = router;
---
+++ public/assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $(".complete").on("click", function(event) {
+        event.preventDefault();
+        const id = $(this).data("id");
+        console.log($(this).data("id"));
+
+        // put request
+        $.ajax({
+            url: "api/todos/" + id,
+        }).then(() => {
+            location.reload();
+        });
+    });
+});
